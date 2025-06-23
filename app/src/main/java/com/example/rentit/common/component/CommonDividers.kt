7d0ad9f71288e11e1d33e8592274453f08fdcf9f
nrj022 <0602smj@gmail.com>
2025-06-23 20:40:20 +0900

    refactor: Rename common component files to plural form

--- app/src/main/java/com/example/rentit/common/component/CommonDividers.kt
+++ app/src/main/java/com/example/rentit/common/component/CommonDividers.kt
@@ -1,16 +1,13 @@
 package com.example.rentit.common.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.rentit.common.theme.Gray100
 import com.example.rentit.common.theme.Gray200
