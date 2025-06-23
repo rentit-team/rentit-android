7e86d61db7b6b0c203fafb583e05c15f1bd1234c
nrj022 <0602smj@gmail.com>
2025-06-23 20:39:00 +0900

    ui: Implement ChatListScreen UI (#7)

--- app/src/main/java/com/example/rentit/common/component/CommonButton.kt
+++ app/src/main/java/com/example/rentit/common/component/CommonButton.kt
@@ -1,12 +1,19 @@
 package com.example.rentit.common.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,10 +40,34 @@ fun CommonButton(text: String, containerColor: Color, contentColor: Color, modif
     }
 }
 
-@Preview
+@Composable
+fun FilterButton(title: String, modifier: Modifier = Modifier, iconContent: @Composable () -> Unit = {}) {
+    OutlinedButton(
+        modifier = modifier.height(30.dp),
+        onClick = { },
+        shape = RoundedCornerShape(20.dp),
+        border = CommonBorders.basicBorder(),
+        contentPadding = PaddingValues(0.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            androidx.compose.material.Text(
+                text = title,
+                style = MaterialTheme.typography.labelLarge)
+            iconContent()
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun ButtonPreview(){
     RentItTheme {
-        CommonButton("Button", PrimaryBlue500, Color.White) {}
+        Column(){
+            CommonButton("Button", PrimaryBlue500, Color.White) {}
+            FilterButton("Button") {}
+        }
     }
 }
--- app/src/main/java/com/example/rentit/feature/chat/ChatListScreen.kt
+++ app/src/main/java/com/example/rentit/feature/chat/ChatListScreen.kt
@@ -1,13 +1,81 @@
 package com.example.rentit.feature.chat
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rentit.R
+import com.example.rentit.common.component.FilterButton
+import com.example.rentit.common.component.screenHorizontalPadding
+import com.example.rentit.common.theme.RentItTheme
+import com.example.rentit.feature.chat.component.ChatListItem
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun ChatListScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ChatListScreen() {
+    Column(Modifier
+        .fillMaxSize()
+        .padding(top = 45.dp)) {
+        Column(
+            modifier = Modifier
+                .screenHorizontalPadding()
+        ) {
+            TopSection()
+            OrderButtonSection()
+        }
+        ChatListSection()
+    }
+}
+
+@Composable
+fun TopSection() {
+    Row {
+        Text(
+            text = stringResource(id = R.string.title_activity_chat_tab),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+fun OrderButtonSection() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 25.dp, bottom = 15.dp),
+        horizontalArrangement = Arrangement.End
+    ) {
+        FilterButton(stringResource(R.string.screen_chat_list_btn_up_to_date_order)) {}
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ChatListSection() {
+    LazyColumn {
+        items(5) {
+            ChatListItem()
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun ChatListScreenPreview() {
+    RentItTheme {
+        ChatListScreen()
+    }
 }
