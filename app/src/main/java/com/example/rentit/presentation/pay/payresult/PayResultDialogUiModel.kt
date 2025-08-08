598ecd7609b4d852cda2be73eaa1ff5d3a78b5a4
nrj022 <0602smj@gmail.com>
2025-08-08 14:57:09 +0900

    style: Spacing fix in PayResultDialogUiModel (#70)

--- app/src/main/java/com/example/rentit/presentation/pay/payresult/PayResultDialogUiModel.kt
+++ app/src/main/java/com/example/rentit/presentation/pay/payresult/PayResultDialogUiModel.kt
@@ -2,5 +2,5 @@ package com.example.rentit.presentation.pay.payresult
 
 data class PayResultDialogUiModel(
     val titleText: String,
-    val contentText: String? =null,
+    val contentText: String? = null,
 )
