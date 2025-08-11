435adc2155b7e68ec0cf67e9b9db0dc30183e2ed
nrj022 <0602smj@gmail.com>
2025-08-11 09:17:54 +0900

    refactor: Remove unused imports in RemovableImageBox.kt (#76)

--- app/src/main/java/com/example/rentit/common/component/item/RemovableImageBox.kt
+++ app/src/main/java/com/example/rentit/common/component/item/RemovableImageBox.kt
@@ -20,8 +20,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
