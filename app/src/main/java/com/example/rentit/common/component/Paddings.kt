46d58fc6ef6820d5f0aefbb917b9b13939a0587f
nrj022 <0602smj@gmail.com>
2025-08-13 16:46:28 +0900

    refactor: Improve JoinNicknameScreen layout and extract components (#85)

--- app/src/main/java/com/example/rentit/common/component/Paddings.kt
+++ app/src/main/java/com/example/rentit/common/component/Paddings.kt
@@ -8,3 +8,8 @@ import androidx.compose.ui.unit.dp
 fun Modifier.screenHorizontalPadding(): Modifier {
     return this.then(Modifier.padding(horizontal = 30.dp))
 }
+
+// 화면 하단에 Button이 고정으로 사용될 경우 필요한 하단 여백을 적용하는 Modifier
+fun Modifier.paddingForBottomBarButton(): Modifier {
+    return this.then(Modifier.padding(bottom = 30.dp))
+}
--- app/src/main/java/com/example/rentit/presentation/auth/join/nickname/JoinNicknameScreen.kt
+++ app/src/main/java/com/example/rentit/presentation/auth/join/nickname/JoinNicknameScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,11 +22,12 @@ import com.example.rentit.R
 import com.example.rentit.common.component.CommonButton
 import com.example.rentit.common.component.CommonTextField
 import com.example.rentit.common.component.CommonTopAppBar
+import com.example.rentit.common.component.paddingForBottomBarButton
 import com.example.rentit.common.component.screenHorizontalPadding
-import com.example.rentit.common.theme.AppRed
 import com.example.rentit.common.theme.PretendardTextStyle
 import com.example.rentit.common.theme.PrimaryBlue500
 import com.example.rentit.common.theme.RentItTheme
+import com.example.rentit.presentation.auth.join.components.InputErrorMessage
 
 @Composable
 fun JoinNicknameScreen(
@@ -36,46 +37,53 @@ fun JoinNicknameScreen(
     onNicknameChange: (String) -> Unit,
     onCompleteClick: () -> Unit
 ) {
-    Column {
-        CommonTopAppBar(
-            title = stringResource(id = R.string.screen_join_title),
-        ) { onBackPressed() }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .screenHorizontalPadding()
-                .padding(bottom = 60.dp),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Spacer(modifier = Modifier.weight(0.5f))
-            HighlightedHeadline()
-            CommonTextField(
-                value = nickname,
-                onValueChange = onNicknameChange,
-                placeholder = stringResource(R.string.app_name)
-            )
-            if (nickname.isBlank() && showNicknameBlankError) {
-                Text(
-                    modifier = Modifier.padding(top = 6.dp, start = 6.dp),
-                    text = stringResource(id = R.string.screen_join_nickname_empty_notification),
-                    color = AppRed,
-                    style = MaterialTheme.typography.labelLarge
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
+    Scaffold(
+        topBar = {
+            CommonTopAppBar(
+                title = stringResource(id = R.string.screen_join_title),
+            ) { onBackPressed() }
+        },
+        bottomBar = {
             CommonButton(
+                modifier = Modifier
+                    .screenHorizontalPadding()
+                    .paddingForBottomBarButton(),
                 text = stringResource(R.string.screen_join_btn_text),
                 containerColor = PrimaryBlue500,
                 contentColor = Color.White
             ) { onCompleteClick() }
         }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .screenHorizontalPadding()
+                .padding(it),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Spacer(modifier = Modifier.weight(0.5f))
+
+            HighlightedHeadline()
+
+            CommonTextField(
+                value = nickname,
+                onValueChange = onNicknameChange,
+                placeholder = stringResource(R.string.app_name)
+            )
+
+            if (nickname.isBlank() && showNicknameBlankError) {
+                InputErrorMessage( stringResource(id = R.string.screen_join_nickname_empty_notification))
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+        }
     }
 }
 
-
 @Composable
-fun HighlightedHeadline() {
+private fun HighlightedHeadline() {
     Text(
         modifier = Modifier.padding(bottom = 27.dp),
         text = buildAnnotatedString {
