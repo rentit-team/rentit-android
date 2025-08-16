3474cf7906db362ca245799397729ace912ee203
nrj022 <0602smj@gmail.com>
2025-08-16 20:50:27 +0900

    refactor: Exclude SharedPreferences from backup and remove error handling in TokenStorage (#93)

--- app/src/main/java/com/example/rentit/common/storage/TokenStorage.kt
+++ app/src/main/java/com/example/rentit/common/storage/TokenStorage.kt
@@ -11,34 +11,27 @@ private const val PREFS_NAME = "secure_prefs"
 private const val ACCESS_TOKEN_KEY = "access_token"
 private const val TAG = "SecurePrefs"
 
-private fun getEncryptedSharedPreference(context: Context): SharedPreferences? {
-    return try {
-        val masterKey = MasterKey.Builder(context)
-            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-            .build()
+private fun getEncryptedSharedPreference(context: Context): SharedPreferences {
+    val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
 
-        EncryptedSharedPreferences.create(
-            context,
-            PREFS_NAME,
-            masterKey,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-    } catch(e: Exception) {
-        Log.e(TAG, "[${e::class.simpleName}] Failed to create EncryptedSharedPreferences")
-        context.deleteSharedPreferences(PREFS_NAME)
-        null
-    }
+    return EncryptedSharedPreferences.create(
+        context,
+        PREFS_NAME,
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
 }
 
 fun saveToken(context: Context, token: String) {
     val sharedPreferences = getEncryptedSharedPreference(context)
-    sharedPreferences?.edit { putString(ACCESS_TOKEN_KEY, token) }
-        ?: Log.w(TAG, "Token not saved due to preference error")
+    sharedPreferences.edit { putString(ACCESS_TOKEN_KEY, token) }
 }
 
 fun getToken(context: Context): String? {
     val sharedPreferences = getEncryptedSharedPreference(context)
-    if(sharedPreferences == null) Log.w(TAG, "Failed to read token due to preference error")
-    return sharedPreferences?.getString(ACCESS_TOKEN_KEY, null)
+    Log.w(TAG, "Failed to read token due to preference error")
+    return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
 }
