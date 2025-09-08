6eac54d2f7fd3ddf37d9f262749170220373077e
nrj022 <0602smj@gmail.com>
2025-09-08 18:24:02 +0900

    ui: Change PENDING status text color to Gray400 (#151)

--- app/src/main/java/com/example/rentit/common/enums/RentalStatus.kt
+++ app/src/main/java/com/example/rentit/common/enums/RentalStatus.kt
@@ -3,13 +3,12 @@ package com.example.rentit.common.enums
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
 import com.example.rentit.R
-import com.example.rentit.common.theme.AppBlack
 import com.example.rentit.common.theme.Gray400
 import com.example.rentit.common.theme.PrimaryBlue500
 import com.example.rentit.common.theme.SecondaryYellow
 
 enum class RentalStatus(@StringRes val strRes: Int, val textColor: Color) {
-    PENDING(R.string.rental_status_pending, AppBlack),
+    PENDING(R.string.rental_status_pending, Gray400),
     ACCEPTED(R.string.rental_status_accepted, PrimaryBlue500),
     REJECTED(R.string.rental_status_rejected, Gray400),
     PAID(R.string.rental_status_paid, PrimaryBlue500),
