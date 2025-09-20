2dcf5ecbca1de458c27719b20114bfee2c5fd370
nrj022 <0602smj@gmail.com>
2025-09-20 20:37:02 +0900

    feat: Add FullImageDialog for single image (#195)
    - Renamed `FullImageDialog` to `FullImagePagerDialog` to clarify its function as a dialog containing a Pager for multiple images.
    - Created a new `FullImageDialog` composable that displays a single image in a dialog.
    - Updated `ProductDetailScreen` to use `FullImagePagerDialog`.

--- app/src/main/java/com/example/rentit/common/component/dialog/FullImagePagerDialog.kt
+++ app/src/main/java/com/example/rentit/common/component/dialog/FullImagePagerDialog.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -20,8 +23,9 @@ import com.example.rentit.common.theme.AppBlack
 import com.example.rentit.common.theme.RentItTheme
 
 @Composable
-fun FullImageDialog(
-    imageUrls: String?,
+fun FullImagePagerDialog(
+    pagerState: PagerState,
+    imageUrls: List<String?>,
     onDismiss: () -> Unit,
 ) {
     Dialog(
@@ -41,23 +45,34 @@ fun FullImageDialog(
                 ) { onDismiss() },
             contentAlignment = Alignment.Center
         ) {
-            LoadableUrlImage(
-                modifier = Modifier.fillMaxHeight(0.7f),
-                imgUrl = imageUrls,
-                defaultImageResId = R.drawable.img_placeholder,
-                defaultDescResId = R.string.screen_product_detail_img_description,
-                contentScale = ContentScale.FillWidth
-            )
+            HorizontalPager(
+                state = pagerState,
+                verticalAlignment = Alignment.CenterVertically
+            ) { page ->
+                LoadableUrlImage(
+                    modifier = Modifier.fillMaxHeight(0.7f),
+                    imgUrl = imageUrls[page],
+                    defaultImageResId = R.drawable.img_placeholder,
+                    defaultDescResId = R.string.screen_product_detail_img_description,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun FullImageDialogPreview() {
+fun FullImagePagerDialogPreview() {
+    val sampleImageUrls =
+        listOf(
+            "https://www.econovill.com/news/photo/201812/353683_236958_5647.jpg",
+            "https://flexible.img.hani.co.kr/flexible/normal/590/590/imgdb/resize/2007/1227/68227042_20071227.jpg"
+            )
     RentItTheme {
-        FullImageDialog(
-            imageUrls = "sampleImageUrls",
+        FullImagePagerDialog(
+            pagerState = rememberPagerState(1) { sampleImageUrls.size },
+            imageUrls = sampleImageUrls,
             onDismiss = {},
         )
     }
