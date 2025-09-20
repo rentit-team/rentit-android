a2e176509d57f7b626c1bd0c76d0022dd53c51d8
nrj022 <0602smj@gmail.com>
2025-09-20 23:03:24 +0900

    refactor: Move WebSocket logic from WebSocketManager to ChatRepository (#199)
    - Modified `ChatRepository` and `ChatRepositoryImpl` to include WebSocket connection, message sending, and disconnection functionalities.
    - Injected `ChatWebSocketDataSource` into `ChatRepositoryImpl` to handle WebSocket operations.
    - This change centralizes chat-related data operations, including WebSocket communication, within the `ChatRepository`, improving code organization and adherence to architectural patterns.

--- app/src/main/java/com/example/rentit/data/chat/repositoryImpl/ChatRepositoryImpl.kt
+++ app/src/main/java/com/example/rentit/data/chat/repositoryImpl/ChatRepositoryImpl.kt
@@ -3,13 +3,16 @@ package com.example.rentit.data.chat.repositoryImpl
 import com.example.rentit.core.network.safeApiCall
 import com.example.rentit.data.chat.dto.ChatDetailResponseDto
 import com.example.rentit.data.chat.dto.ChatListResponseDto
+import com.example.rentit.data.chat.dto.MessageResponseDto
 import com.example.rentit.data.chat.dto.NewChatResponseDto
 import com.example.rentit.data.chat.remote.ChatRemoteDataSource
+import com.example.rentit.data.chat.remote.ChatWebSocketDataSource
 import com.example.rentit.domain.chat.repository.ChatRepository
 import javax.inject.Inject
 
 class ChatRepositoryImpl @Inject constructor(
     private val chatRemoteDataSource: ChatRemoteDataSource,
+    private val chatWebSocketDataSource: ChatWebSocketDataSource
 ): ChatRepository {
 
     override suspend fun getChatList(): Result<ChatListResponseDto> {
@@ -22,5 +25,26 @@ class ChatRepositoryImpl @Inject constructor(
 
     override suspend fun postNewChat(productId: Int): Result<NewChatResponseDto> {
         return safeApiCall { chatRemoteDataSource.postNewChat(productId) }
+    }
+
+    override fun connectWebSocket(chatRoomId: String, onMessageReceived: (MessageResponseDto) -> Unit, onError: (Throwable) -> Unit) {
+        chatWebSocketDataSource.connect(
+            chatRoomId = chatRoomId,
+            onMessageReceived = onMessageReceived,
+            onError = onError
+        )
+    }
+
+    override fun sendMessage(chatRoomId: String, message: String, onSuccess: () -> Unit, onError: (Throwable) -> Unit) {
+        chatWebSocketDataSource.sendMessage(
+            chatRoomId = chatRoomId,
+            message = message,
+            onSuccess = onSuccess,
+            onError = onError
+        )
+    }
+
+    override fun disconnectWebSocket() {
+        chatWebSocketDataSource.disconnect()
     }
 }
